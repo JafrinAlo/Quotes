d524524e596c0e19333005451892d6d37a89196b
Jafrin Akter Alo <81787520+JafrinAlo@users.noreply.github.com>
2022-07-22 23:39:52 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+        <title> Quotes </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1>Rich/Poor bro Quotes</h1>
+        <h2>Made for fun purpose</h2>
+       
+        <div class="container">
+        <form action="">
+            <label for="" id="search">Search Quotes</label>
+            <input type="text" id="search">
+        </form>
+            <div class="row">
+            <div class="col-md-6 left">
+                <h2>Rich Bro once said:</h2>
+                <p></p>
+                <button type =submit class="btn btn-primary" value="prev_r">Previous Quotes</button>
+
+            </div>
+            <div class="col-md-6 right">
+                <h2>Poor Bro once said:</h2>
+                <p></p>
+                <button type =submit class="btn btn-primary" value="prev_r">Previous Quotes</button>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
